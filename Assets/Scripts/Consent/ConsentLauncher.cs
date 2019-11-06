27f6c2c74d55a77ecee1d1a934a819d49048d52d
Juha Liias <juha.liias@gmail.com>
2019-11-07 01:00:48 +0200

    easymobile privacy initial version

---
+++ Assets/Scripts/Consent/ConsentLauncher.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class ConsentLauncher : MonoBehaviour
+{
+    private void Start()
+    {
+        LaunchConsent();
+    }
+
+    public void LaunchConsent() {
+        this.GetComponentInParent<EasyMobile.WSConsent.WSPrivacy>().ShowConsentDialog();
+    }
+}
